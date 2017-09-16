8de8c94db8cb5df7964a1017985e7ed2bfc0d241
Qi Tan <qtan@ualberta.ca>
2017-09-16 03:03:24 -0600

    css,js

--- Titan/src/db/mongodb/MongoDBConnection.java
+++ Titan/src/db/mongodb/MongoDBConnection.java
@@ -1,18 +1,25 @@
 package db.mongodb;
 
+//This line needs manual import.
+import static com.mongodb.client.model.Filters.eq;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.UpdateOptions;
 
 import db.DBConnection;
 import entity.Item;
+import entity.Item.*;
 import external.ExternalAPI;
 import external.ExternalAPIFactory;
+
 
 public class MongoDBConnection implements DBConnection {
 
@@ -43,33 +50,76 @@ public class MongoDBConnection implements DBConnection {
 
 	@Override
 	public void setFavoriteItems(String userId, List<String> itemIds) {
-		// TODO Auto-generated method stub
-		
+		db.getCollection("users").updateOne(new Document("user_id", userId),
+				new Document("$push", new Document("favorite", new Document("$each", itemIds))));
 	}
 
 	@Override
 	public void unsetFavoriteItems(String userId, List<String> itemIds) {
-		// TODO Auto-generated method stub
-		
+		db.getCollection("users").updateOne(new Document("user_id", userId),
+				new Document("$pullAll", new Document("favorite", itemIds)));
+
 	}
+
+
+
 
 	@Override
 	public Set<String> getFavoriteItemIds(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<String> favoriteItems = new HashSet<String>();
+		// db.users.find({user_id:1111})
+		FindIterable<Document> iterable = db.getCollection("users").find(eq("user_id", userId));
+		if (iterable.first().containsKey("favorite")) {
+			@SuppressWarnings("unchecked")
+			List<String> list = (List<String>) iterable.first().get("favorite");
+			favoriteItems.addAll(list);
+		}
+		return favoriteItems;
 	}
+
 
 	@Override
 	public Set<Item> getFavoriteItems(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		Set<String> itemIds = getFavoriteItemIds(userId);
+		Set<Item> favoriteItems = new HashSet<>();
+		for (String itemId : itemIds) {
+			FindIterable<Document> iterable = db.getCollection("items").find(eq("item_id", itemId));
+			Document doc = iterable.first();
+			ItemBuilder builder = new ItemBuilder();
+			builder.setItemId(doc.getString("item_id"));
+			builder.setName(doc.getString("name"));
+			builder.setCity(doc.getString("city"));
+			builder.setState(doc.getString("state"));
+			builder.setCountry(doc.getString("country"));
+			builder.setZipcode(doc.getString("zipcode"));
+			builder.setRating(doc.getDouble("rating"));
+			builder.setAddress(doc.getString("address"));
+			builder.setLatitude(doc.getDouble("latitude"));
+			builder.setLongitude(doc.getDouble("longitude"));
+			builder.setDescription(doc.getString("description"));
+			builder.setSnippet(doc.getString("snippet"));
+			builder.setSnippetUrl(doc.getString("snippet_url"));
+			builder.setImageUrl(doc.getString("image_url"));
+			builder.setUrl(doc.getString("url"));
+			favoriteItems.add(builder.build());
+		}
+		return favoriteItems;
 	}
 
+
 	@Override
-	public Set<String> getCategories(String itemId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<String> getCategories(String itemId) {//Student Question
+		Set<String> categories = new HashSet<>();
+		FindIterable<Document> iterable = db.getCollection("items").find(eq("item_id", itemId));
+
+		if (iterable.first().containsKey("categories")) {
+			@SuppressWarnings("unchecked")
+			List<String> list = (List<String>) iterable.first().get("categories");
+			categories.addAll(list);
+		}
+		return categories;
 	}
+
 
 	@Override
 	public void saveItem(Item item) {
--- Titan/src/rpc/SearchItem.java
+++ Titan/src/rpc/SearchItem.java
@@ -1,6 +1,7 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -10,67 +11,93 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import db.DBConnection;
 import db.DBConnectionFactory;
 import entity.Item;
+import external.ExternalAPI;
+import external.ExternalAPIFactory;
 
 /**
  * Servlet implementation class SearchItem
+ * rpc: framework
  */
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-  private DBConnection conn = DBConnectionFactory.getDBConnection();
-
-  /**
-   * Default constructor.
-   */
-  public SearchItem() {
-    // TODO Auto-generated constructor stub
-  }
-
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    String userId = request.getParameter("user_id");
-    double lat = Double.parseDouble(request.getParameter("lat"));
-    double lon = Double.parseDouble(request.getParameter("lon"));
-    // Term can be empty or null.
-    String term = request.getParameter("term");
-    List<Item> items = conn.searchItems(userId, lat, lon, term);
-    List<JSONObject> list = new ArrayList<>();
-
-    Set<String> favorite = conn.getFavoriteItemIds(userId);
-    try {
-      for (Item item : items) {
-        // Add a thin version of restaurant object
-        JSONObject obj = item.toJSONObject();
-        // Check if this is a favorite one.
-        // This field is required by frontend to correctly display favorite items.
-        obj.put("favorite", favorite.contains(item.getItemId()));
-
-        list.add(obj);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+	private static final long serialVersionUID = 1L;
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
     }
-    JSONArray array = new JSONArray(list);
-    RpcHelper.writeJsonArray(response, array);
-  }
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-   */
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    doGet(request, response);
-  }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+
+    		String userId = request.getParameter("user_id");
+    		double lat = Double.parseDouble(request.getParameter("lat"));
+    		double lon = Double.parseDouble(request.getParameter("lon"));
+    		// Term can be empty or null.
+    		String term = request.getParameter("term");
+    		List<Item> items = conn.searchItems(userId, lat, lon, term);
+    		List<JSONObject> list = new ArrayList<>();
+
+    		Set<String> favorite = conn.getFavoriteItemIds(userId);
+    		try {
+    			for (Item item : items) {
+    				// 	Add a thin version of restaurant object
+    				JSONObject obj = item.toJSONObject();
+    				// Check if this is a favorite one.
+    				// This field is required by frontend to correctly display favorite items.
+    				obj.put("favorite", favorite.contains(item.getItemId()));
+    				list.add(obj);
+    			}
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    			JSONArray array = new JSONArray(list);
+    			RpcHelper.writeJsonArray(response, array);
+    		}
+    
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		String userId = request.getParameter("user_id");
+//		double lat = Double.parseDouble(request.getParameter("lat"));
+//		double lon = Double.parseDouble(request.getParameter("lon"));
+//		// Term can be empty or null.
+//		String term = request.getParameter("term");
+//		List<Item> items = conn.searchItems(userId, lat, lon, term);
+//		List<JSONObject> list = new ArrayList<>();
+//		try {
+//			for (Item item : items) {
+//				JSONObject obj = item.toJSONObject();
+//				list.add(obj);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		JSONArray array = new JSONArray(list);
+//		RpcHelper.writeJsonArray(response, array);
+//		
+//	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
