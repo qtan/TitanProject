3e838dd9403d0d6a82cb8e879d4beadf34e48891
Qi Tan <qtan@ualberta.ca>
2017-09-16 05:49:23 -0600

    get/add item

--- Titan/WebContent/scripts/main.js
+++ Titan/WebContent/scripts/main.js
@@ -21,6 +21,106 @@
 
     return element;
   }
+  function listItems(items) {
+		// Clear the current results
+		var itemList = $('item-list');
+		itemList.innerHTML = '';
+
+		for (var i = 0; i < items.length; i++) {
+			addItem(itemList, items[i]);
+		}
+	}
+  function addItem(itemList, item) {
+		var item_id = item.item_id;
+
+		// create the <li> tag and specify the id and class attributes
+		var li = $('li', {
+			id : 'item-' + item_id,
+			className : 'item'
+		});
+
+		// set the data attribute
+		li.dataset.item_id = item_id;
+		li.dataset.favorite = item.favorite;
+
+		// item image
+		if (item.image_url) {
+			li.appendChild($('img', {
+				src : item.image_url
+			}));
+		} else {
+			li.appendChild($('img', {
+				src : 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
+			}))
+		}
+		// section
+		var section = $('div', {});
+
+		// title
+		var title = $('a', {
+			href : item.url,
+			target : '_blank',
+			className : 'item-name'
+		});
+		title.innerHTML = item.name;
+		section.appendChild(title);
+
+		// category
+		var category = $('p', {
+			className : 'item-category'
+		});
+		category.innerHTML = 'Category: ' + item.categories.join(', ');
+		section.appendChild(category);
+
+		var stars = $('div', {
+			className : 'stars'
+		});
+		
+		for (var i = 0; i < item.rating; i++) {
+			var star = $('i', {
+				className : 'fa fa-star'
+			});
+			stars.appendChild(star);
+		}
+
+		if (('' + item.rating).match(/\.5$/)) {
+			stars.appendChild($('i', {
+				className : 'fa fa-star-half-o'
+			}));
+		}
+
+		section.appendChild(stars);
+
+		li.appendChild(section);
+
+		// address
+		var address = $('p', {
+			className : 'item-address'
+		});
+
+		address.innerHTML = item.address.replace(/,/g, '<br/>').replace(/\"/g,
+				'');
+		li.appendChild(address);
+
+		// favorite link
+		var favLink = $('p', {
+			className : 'fav-link'
+		});
+
+		favLink.onclick = function() {
+			changeFavoriteItem(item_id);
+		};
+
+		favLink.appendChild($('i', {
+			id : 'fav-icon-' + item_id,
+			className : item.favorite ? 'fa fa-heart' : 'fa fa-heart-o'
+		}));
+
+		li.appendChild(favLink);
+
+		itemList.appendChild(li);
+	}
+
 
   function showLoadingMessage(msg) {
     var itemList = $('item-list');
